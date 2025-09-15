adf2ca4d2bcf8bcdae5df9bc35ec5d135118c778
osmartinez <its.osmartinez@gmail.com>
2025-09-15 21:31:11 +0200

    feat: implement error htow when robot overflows some limits

--- src/main/kotlin/domain/Floor.kt
+++ src/main/kotlin/domain/Floor.kt
@@ -1,5 +1,9 @@
 package domain
 
 data class Floor (val width: Int, val height: Int) {
-
+    init {
+        require(width >= 0 && height >= 0)
+    }
+    fun contains(p: Position): Boolean =
+        p.x in 0..width && p.y in 0..height
 }
--- src/main/kotlin/domain/Robot.kt
+++ src/main/kotlin/domain/Robot.kt
@@ -1,8 +1,14 @@
 package domain
 
+import domain.exceptions.DomainException
+
 data class Robot (val position: Position, val orientation: Orientation, val floor: Floor){
 
     fun rotateClockwise(): Robot = copy(orientation= orientation.clockwise())
     fun rotateAnticlockwise(): Robot = copy(orientation= orientation.anticlockwise())
-    fun move(): Robot = copy(position = position.forward(orientation))
+    fun move(): Robot {
+        val next = position.forward(orientation)
+        if(!floor.contains(next)) throw DomainException("Robot is trying to move outside the floor")
+        return copy(position = next)
+    }
 }
