9025a2630c989fc0cfe082cc9182e2fe2938406d
osmartinez <its.osmartinez@gmail.com>
2025-09-15 22:51:26 +0200

    test: add failing test to inject and use a logger

--- src/test/kotlin/application/RunInstructionsToRobotTest.kt
+++ src/test/kotlin/application/RunInstructionsToRobotTest.kt
@@ -7,12 +7,21 @@ import domain.Position
 import domain.Robot
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Assertions.assertTrue;
+
+private class FakeLogger: DomainLogger{
+    val logs = mutableListOf<String>()
+    override fun info(log: String) { logs += log}
+}
 
 class RunInstructionsToRobotTest {
 
     @Test
     fun `run instructions to robot`() {
-        val useCase = RunInstructionsToRobot()
+
+        val logger = FakeLogger()
+
+        val useCase = RunInstructionsToRobot(logger)
         val floor = Floor(5, 5)
         val robot = Robot(Position(3, 3), Orientation.E, floor)
         val instructionList = InstructionList.parse("MMRMMRMRRM")
@@ -20,5 +29,8 @@ class RunInstructionsToRobotTest {
         assertEquals(robot1.position, Position(5,1))
         assertEquals(robot1.orientation,Orientation.E)
 
+        assertTrue(logger.logs.any { it.contains("Starting position: (3, 3)") })
+        assertTrue(logger.logs.any { it.contains("Final position: (5, 1)") })
+        assertTrue(logger.logs.any { it.contains("Final orientation: E") })
     }
 }
