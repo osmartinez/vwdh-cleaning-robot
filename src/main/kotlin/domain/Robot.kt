63721bfb82f59e2ae83f2e247df227d98386eea6
osmartinez <its.osmartinez@gmail.com>
2025-09-16 00:17:06 +0200

    feat: creates new ctor to generate uuid

--- src/main/kotlin/domain/Robot.kt
+++ src/main/kotlin/domain/Robot.kt
@@ -3,16 +3,30 @@ package domain
 import domain.exceptions.DomainException
 import java.util.UUID
 
-data class Robot (val position: Position, val orientation: Orientation, val floor: Floor){
-    override fun toString(): String {
-        return "${position.x} ${position.y} ${orientation.name}"
-    }
-    fun rotateClockwise(): Robot = copy(orientation= orientation.clockwise())
-    fun rotateAnticlockwise(): Robot = copy(orientation= orientation.anticlockwise())
+data class Robot(
+    val id: RobotId,
+    val position: Position,
+    val orientation: Orientation,
+    val floor: Floor
+) {
+    override fun toString(): String =
+        "${position.x} ${position.y} ${orientation.name}"
+
+    fun rotateClockwise(): Robot =
+        copy(orientation = orientation.clockwise())
+
+    fun rotateAnticlockwise(): Robot =
+        copy(orientation = orientation.anticlockwise())
+
     fun move(): Robot {
         val next = position.forward(orientation)
-        if(!floor.contains(next)) throw DomainException("Robot is trying to move outside the floor")
+        if (!floor.contains(next)) throw DomainException("Robot is trying to move outside the floor")
         return copy(position = next)
+    }
+
+    companion object {
+        fun create(position: Position, orientation: Orientation, floor: Floor): Robot =
+            Robot(RobotId.new(), position, orientation, floor)
     }
 }
 
