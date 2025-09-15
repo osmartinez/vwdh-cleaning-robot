e59c89ec2f367ce4456a6f238f49355244d0e81d
osmartinez <its.osmartinez@gmail.com>
2025-09-15 21:07:54 +0200

    feat: implement Position forward method for remaining orientations

--- src/main/kotlin/domain/Position.kt
+++ src/main/kotlin/domain/Position.kt
@@ -4,6 +4,8 @@ data class Position (val x: Int, val y: Int) {
 
     fun forward(orientation: Orientation) = when (orientation) {
         Orientation.N -> copy(y = y + 1)
-        else -> copy(x = x + 1)
+        Orientation.E -> copy(x = x + 1)
+        Orientation.S -> copy(y = y - 1)
+        Orientation.W -> copy(x = x - 1)
     }
 }
