6080fb7ffa1fb8a92a67140a72183945929c52e4
osmartinez <its.osmartinez@gmail.com>
2025-09-15 20:55:02 +0200

    test: add failing test for remaining rotations

--- src/test/kotlin/domain/OrientationTest.kt
+++ src/test/kotlin/domain/OrientationTest.kt
@@ -6,4 +6,11 @@ import org.junit.jupiter.api.Assertions.assertEquals;
 class OrientationTest {
     @Test
     fun `clockwise from N to E`() = assertEquals(Orientation.E, Orientation.N.clockwise())
+    @Test
+    fun `clockwise from E to S`() = assertEquals(Orientation.S, Orientation.E.clockwise())
+    @Test
+    fun `clockwise from S to W`() = assertEquals(Orientation.W, Orientation.S.clockwise())
+    @Test
+    fun `clockwise from W to N`() = assertEquals(Orientation.N, Orientation.W.clockwise())
+
 }
