e634686ec9b26c7a73a90d1b5f03b5567db13308
osmartinez <its.osmartinez@gmail.com>
2025-09-15 21:07:15 +0200

    test: add failing test for Position forward method on remaining orientations

--- src/test/kotlin/domain/PositionTest.kt
+++ src/test/kotlin/domain/PositionTest.kt
@@ -9,4 +9,22 @@ class PositionTest {
         val current = Position(1,1).forward(Orientation.N)
         assertEquals(Position(1,2), current)
     }
+
+    @Test
+    fun `forward E`(){
+        val current = Position(1,1).forward(Orientation.E)
+        assertEquals(Position(2,1), current)
+    }
+
+    @Test
+    fun `forward S`(){
+        val current = Position(1,1).forward(Orientation.S)
+        assertEquals(Position(1,0), current)
+    }
+
+    @Test
+    fun `forward W`(){
+        val current = Position(1,1).forward(Orientation.W)
+        assertEquals(Position(0,1), current)
+    }
 }
