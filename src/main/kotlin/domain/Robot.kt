417cb6684446fbb1af2648c962c12880f89003bc
osmartinez <its.osmartinez@gmail.com>
2025-09-15 21:22:05 +0200

    feat: implement robot move method

--- src/main/kotlin/domain/Robot.kt
+++ src/main/kotlin/domain/Robot.kt
@@ -4,4 +4,5 @@ data class Robot (val position: Position, val orientation: Orientation, val floo
 
     fun rotateClockwise(): Robot = copy(orientation= orientation.clockwise())
     fun rotateAnticlockwise(): Robot = copy(orientation= orientation.anticlockwise())
+    fun move(): Robot = copy(position = position.forward(orientation))
 }
